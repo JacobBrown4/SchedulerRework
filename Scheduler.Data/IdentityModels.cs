160d25f01d5eccec8152ed9532181c5f734506a5
Josh Shelton <joshshelton.sd@gmail.com>
2021-12-08 10:51:40 -0500

    pull from dev

--- Scheduler.Data/IdentityModels.cs
+++ Scheduler.Data/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNetCore.Identity;
 using Scheduler.Data;
 
 namespace SchedulerMVP.Data
@@ -48,6 +50,11 @@ namespace SchedulerMVP.Data
                 .Configurations
                 .Add(new IdentityUserLoginConfiguration())
                 .Add(new IdentityUserRoleConfiguration());
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
         }
     }
 
--- Scheduler.Services/AppointmentService.cs
+++ Scheduler.Services/AppointmentService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Scheduler.Data;
+using Scheduler.Models.AppointmentModels;
+using SchedulerMVP.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,25 @@ namespace Scheduler.Services
 {
     public class AppointmentService
     {
+        private readonly Guid _userId;
+        public AppointmentService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateAppointment(AppointmentCreate model)
+        {
+            var entity =
+                new Appointment()
+                {
+                    ClientId = model.ClientId,
+                    EmployeeId = model.EmployeeId
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Enrollment.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
